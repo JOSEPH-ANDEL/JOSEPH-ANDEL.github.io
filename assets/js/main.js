567f7dc6d5b40285aa1dee1f42e582b19ae73018
JOSEPH-ANDEL <155003094+JOSEPH-ANDEL@users.noreply.github.com>
2023-12-27 20:40:27 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,8 +81,8 @@
 					// Scrollex.
 						$section.scrollex({
 							mode: 'middle',
-							top: '-20vh',
-							bottom: '-20vh',
+							top: '-30vh',
+							bottom: '-30vh',
 							initialize: function() {
 
 								// Deactivate section.
