3964f4c0232bc141ff787ce0f0fe998f1265d6c5
JOSEPH-ANDEL <155003094+JOSEPH-ANDEL@users.noreply.github.com>
2025-06-16 07:32:35 -0400

    Updated Info

--- src/components/Expertise.tsx
+++ src/components/Expertise.tsx
@@ -1,70 +1,58 @@
 import React from "react";
 import '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faReact, faDocker, faPython } from '@fortawesome/free-brands-svg-icons';
+import {faGithub, faPython } from '@fortawesome/free-brands-svg-icons';
 import Chip from '@mui/material/Chip';
 import '../assets/styles/Expertise.scss';
 
-const labelsFirst = [
-    "React",
-    "TypeScript",
-    "JavaScript",
-    "HTML5",
-    "CSS3",
-    "SASS",
-    "Flask",
-    "Python",
-    "SQL",
-    "PostgreSQL",
-    "Postman"
-];
+
 
 const labelsSecond = [
-    "Git",
-    "GitHub Actions",
-    "Docker",
-    "AWS",
-    "Azure",
-    "Linux",
-    "Snowflake",
-    "Pandas",
-    "Selenium",
+    "C",
+    "C++",
+    "C#",
+    "Python",
+    "Java",
+    "Javascript",
+    "Typescript",
+    "AMPL",
 ];
 
 const labelsThird = [
-    "OpenAI",
-    "Groq",
-    "LangChain",
-    "Qdrant",
-    "Hugging Face",
-    "LlamaIndex",
-    "Streamlit",
+    "Git",
+    "HTML",
+    "CSS",
+    "React",
+    "Machine Learning",
+    "Pandas",
+    "NumPy",
+    "SQL",
+    "MongoDB",
+    "Firebase"
 ];
 
 function Expertise() {
     return (
     <div className="container" id="expertise">
         <div className="skills-container">
-            <h1>Expertise</h1>
+            <h1>Expertise and Education</h1>
             <div className="skills-grid">
                 <div className="skill">
-                    <FontAwesomeIcon icon={faReact} size="3x"/>
-                    <h3>Full Stack Web Development</h3>
-                    <p>I have built a diverse array of web applications from scratch using modern technologies such as React and Flask. I have a strong proficiency in the SDLC process and frontend + backend development.</p>
-                    <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
-                        {labelsFirst.map((label, index) => (
-                            <Chip key={index} className='chip' label={label} />
-                        ))}
-                    </div>
+                    <img
+                      src={`${process.env.PUBLIC_URL}/George-Mason-University-Symbol.png`}
+                      style={{ width: 85, height: 48 }}
+                    />
+                    <h3>B.S. in Computer Science</h3>
+                    <p>I graduated Cum Laude from George Mason University in 2025 with a final GPA of 3.53</p>
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faDocker} size="3x"/>
-                    <h3>DevOps & Automation</h3>
-                    <p>Once the application is built, I help clients set up DevOps testing, CI/CD pipelines, and deployment automation to support the successful Go-Live.</p>
+                    <FontAwesomeIcon icon={faPython} size="3x"/>
+                    <h3>Programming Languages</h3>
+                    <p>Over the years I have picked up skills in a wide variety of OOP programming languages such as Java, Python, C++, C#, Javascript, and Typescript. I also have 
+                        experience with non OOP languages such as C and AMPL.</p>
                     <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
+                        <span className="chip-title">Languages:</span>
                         {labelsSecond.map((label, index) => (
                             <Chip key={index} className='chip' label={label} />
                         ))}
@@ -72,11 +60,13 @@ function Expertise() {
                 </div>
 
                 <div className="skill">
-                    <FontAwesomeIcon icon={faPython} size="3x"/>
-                    <h3>GenAI & LLM</h3>
-                    <p>Stay relevant in the market by leveraging the latest AI models in your projects. I have professional experience building enterprise grade GenAI-enabled solutions to empower intelligent decision making.</p>
+                    <FontAwesomeIcon icon={faGithub} size="3x"/>
+                    <h3>Technical Skills</h3>
+                    <p>I have acquired a wide variety of technical skills not directly tied to programming languages including Git,
+                        HTML, CSS, React, and various data science libraries such as Pandas and NumPy. I also have experience with databases such as SQL, MongoDB, and Firebase.
+                    </p>
                     <div className="flex-chips">
-                        <span className="chip-title">Tech stack:</span>
+                        <span className="chip-title">Technical Skills:</span>
                         {labelsThird.map((label, index) => (
                             <Chip key={index} className='chip' label={label} />
                         ))}
